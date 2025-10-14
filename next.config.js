a8a3acf7a1ab7fb68e3895f3953e873823fcb9ed
TarunpreetSingh23 <tarunpreets29@gmail.com>
2025-10-14 23:57:25 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // Add remotePatterns to whitelist specific external domains
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'example.com',
-        port: '', // Leave empty if default ports (80/443) are used
-        pathname: '/images/**', // Allow any path under /images on this host
+        port: '',
+        pathname: '/images/**',
       },
     ],
-    // If you were using a simple list of domains:
-    // domains: ['example.com'], // This is the older format, remotePatterns is recommended
   },
 };
 
