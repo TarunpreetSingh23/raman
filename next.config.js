6ded8232934c30a0d08acebe45a8fff51691e337
TarunpreetSingh23 <tarunpreets29@gmail.com>
2025-10-15 09:02:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'example.com',
-        port: '',
-        pathname: '/images/**',
-      },
-    ],
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
